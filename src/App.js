f0d9ca7470619f2b1b80e7b260cf84bdcb25b9bf
Ron Martin <ronmartin89@gmail.com>
2020-02-07 13:21:33 -0800

    Added notes and made App a class

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Test</h1>
-    </div>
-  );
+// Users should be able to search for a character name
+// App will make an API request to the Star Wars API
+// with the response, render it out to the user
+// display Matching Characters for their search. 
+//    ex. search "Skywalker"
+//    should RETURN "Luke Skywalker", "Anakin Skywalker", "Shmi Skywalker", etc
+// --MUST deploy to Zeits Now
+
+// BONUS:
+// -Gracefully displaying a loading indicator 
+//    and handling error conditions (validation, error rendering)
+// -Be able to SELECT whether they want to search for planets, spaceships, vehicles, characters,
+//    films, or species
+// - A passing test suite
+
+class App extends React.Component {
+
+  // For Fetch. Search people by this: where r2 is the string you wish to search.
+  // https://swapi.co/api/people/?search=r2
+
+  state = {};
+
+  componentDidMount() {
+
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Test</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
